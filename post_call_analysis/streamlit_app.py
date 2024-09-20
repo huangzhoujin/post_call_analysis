e8c9cb490edd3d2e556d255443f8430c4de016e3
Huang Zhoujin <huangzhoujin@hotmail.com>
2024-09-20 21:03:21 +0800

    Initial project

---
+++ post_call_analysis/streamlit_app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Project :   post_call_analysis
+@File    :   streamlit_app.py
+@Contact :   zhoujin.huang@dlambda.net
+@Revised              @Author           @Version    @Desciption
+------------------    --------------    --------    -----------
+9/5/2024 9:22 AM       Zhoujin Huang     1.0.0       None
+'''
